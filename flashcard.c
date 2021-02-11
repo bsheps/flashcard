729afbc9cf276911aa32d50537febe29cf5c1cfb
Brandon <brandon.shepard6@icloud.com>
2021-02-11 10:43:42 -0600

    tab delimited csv converted into flashcard LL

--- flashcard.c
+++ flashcard.c
@@ -10,9 +10,11 @@ struct Flashcard{
     char *note;
     char *deck;
     Flashcard *next;
+    Flashcard *prev;
 };
 
 Flashcard *makecardsfromcsv(FILE *fp,char *deck);
+Flashcard *buildFlashcard(char *note, char *deck);
 
 int main(int argc, char *argv[]){
     char *fname;
@@ -29,16 +31,23 @@ int main(int argc, char *argv[]){
             exit(2);
         }else{
             root = makecardsfromcsv(fp, fname);             
-
         }
+    }
+    while(root != NULL){
+        fprintf(stdout, "name: %s\nnote: %s\n",root->deck, root->note);
+        root= root->next;
     }
 }
 
+/* take a tab delimited csv file, read it line by line and produce flashcards
+   in a linked list. Return the head of the list */
 Flashcard *makecardsfromcsv(FILE *fp,char *deck){
-    char line[MAXLINE], *f, *out, *temp;;
+    char line[MAXLINE], *f, *out, *temp, *filename;
     Flashcard *root, *head; 
-    head = root;
+    head = root = NULL;
     int multinote = FALSE;
+    filename = (char *)malloc((strlen(deck)+1) * sizeof(char));
+    strcpy(filename, deck);
     do{
         f = fgets(line, MAXLINE, fp);
         if(!multinote && line[0] == '"'){
@@ -60,16 +69,40 @@ Flashcard *makecardsfromcsv(FILE *fp,char *deck){
                 out = temp;
                 if(line[strlen(line)-2] == '"'){
                     multinote = FALSE;
-                    fprintf(stdout, "\nmulti-begin\n%s\nmulti-end\n", out);
+                    if(root == NULL){
+                        head = root = buildFlashcard(out, filename);
+
+                    }else{
+                        root->next = buildFlashcard(out, filename);
+                        root->next->prev = root;
+                        root = root->next;
+                    }
                 }
             }
         }else{
             out = (char *)malloc((strlen(line)+1) * sizeof(char));
             strcpy(out, line);
-            fprintf(stdout, "%s", out);
+            if(root == NULL){
+                head = root = buildFlashcard(out, filename);
+            }else{
+                root->next = buildFlashcard(out, filename);
+                root->next->prev = root;
+                root = root->next;            
+            }
         }
     }while(f == line);
+
     return head;
 }
 
-
+/* allocates memory and creates new flashcard. Note and Deck are expected to
+ * already have memory allocated */
+Flashcard *buildFlashcard(char *note, char *deck){
+    Flashcard *card;
+    card = (Flashcard *) malloc(sizeof(Flashcard));
+    card->note = note;
+    card->deck = deck;
+    card->next = NULL;
+    card->prev = NULL;
+    return card;
+}
